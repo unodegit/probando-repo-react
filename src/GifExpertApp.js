d156ff6af3964bad28a48e8b0037e2635840d8ef
Brais López <hola@unodetantos.com>
2024-02-06 16:29:31 +0100

    First commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+
+    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball']
+    const [categories, setCategories] = useState (['Goku']);
+
+    // const handleAdd = () => {
+    //     setCategories( [...categories,'HunterXHunter'] );
+    // }
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={ setCategories }/>
+            <hr />
+
+            {/* <button onClick = { handleAdd }>Agregar</button> */}
+
+            <ol>
+                { 
+                    categories.map( category => (
+                        <GifGrid 
+                            key = {category}
+                            category = {category}
+                        />
+                    )) 
+                }
+            </ol>
+        </>
+    )
+
+}
